82a9787fb9b48aac93d078f5e1c6537edb786829
3A <524019351@qq.com>
2025-06-25 12:41:14 +0800

    添加地形

--- Source/forest/Generator.cpp
+++ Source/forest/Generator.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Generator.h"
+#include "Landscape.h"
 
 // Sets default values
 AGenerator::AGenerator()
@@ -10,6 +11,27 @@ AGenerator::AGenerator()
 	PrimaryActorTick.bCanEverTick = true;
 	HISM = CreateDefaultSubobject<UHierarchicalInstancedStaticMeshComponent>(TEXT("HISM"));
 	HISM->SetupAttachment(RootComponent);
+}
+
+float AGenerator::GetLandscapeHeightAtLocation(int X, int Y)
+{
+	UWorld* World = GetWorld();
+    // 从一个很高的位置开始向下射线
+    FVector Start = FVector(X, Y, 10000.f);
+    FVector End = FVector(X, Y, -10000.f);
+
+    FHitResult HitResult;
+    bool bHit = World->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility);
+
+    if (bHit && HitResult.Actor.IsValid())
+    {
+        // 判断是否击中了Landscape
+        if (HitResult.Actor->IsA(ALandscape::StaticClass()))
+        {
+            return HitResult.Location.Z;
+        }
+    }
+	return 0.f;
 }
 
 // Called when the game starts or when spawned
@@ -25,7 +47,8 @@ void AGenerator::BeginPlay()
 	{
 		int X = (int)FMath::RandRange(-20000, 20000);
 		int Y = (int)FMath::RandRange(-20000, 20000);
-		FTransform InstanceTransform(FVector(X, Y, 0));
+		int Z = GetLandscapeHeightAtLocation(X, Y);
+		FTransform InstanceTransform(FVector(X, Y, Z));
 		int32 InstanceIndex = HISM->AddInstance(InstanceTransform);
 	}
 }
--- Source/forest/Generator.h
+++ Source/forest/Generator.h
@@ -22,6 +22,7 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	float GetLandscapeHeightAtLocation(int X, int Y);
 
 public:	
 	// Called every frame
